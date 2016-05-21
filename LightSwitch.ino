991c86e2b7bae2fba91db8e14ab8c2cf1d49d1bd
Wene <wene83@gmx.ch>
2016-05-21 13:59:52 +0200

    default timeout now doubled and blinking led

--- LightSwitch.ino
+++ LightSwitch.ino
@@ -2,7 +2,8 @@
 #define LIGHT2 6
 #define BTN1 9
 #define BTN2 8
-#define TICKER 6000
+#define LED 13
+#define TICKER 12000    // TICKER * DELAY => Timeout in ms
 #define DELAY 10
 
 #include "switch.h"
@@ -12,12 +13,25 @@ Switch Sw2(LIGHT2, BTN2, BTN1, TICKER, DELAY);
 
 void setup()
 {
-    //so far nothing here...
+    pinMode(OUTPUT, LED);
 }
+
+int ledCount = 0;
+bool ledOn = true;
 
 void loop()
 {
     Sw1.handle();
     Sw2.handle();
+
+    // Blink the LED
+    ledCount++;
+    if(ledCount > 100)
+    {
+        ledCount = 0;
+        ledOn = !ledOn;
+        digitalWrite(LED, ledOn);
+    }
+    
     delay(DELAY);
 }
